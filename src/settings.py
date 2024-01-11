768c1618f33a9b515222f5aed1ad4b13de39fd52
peixed351 <peixed351@gmail.com>
2024-01-11 20:32:01 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -36,7 +36,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://mucar3.de/icpr2020-tas500/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 12520106
+PREVIEW_IMAGE_ID: int = 12529293
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/tas500"
